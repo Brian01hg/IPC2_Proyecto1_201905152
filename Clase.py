44f351240887d1f73c7613b45129d27fc2a0f54b
Brian01hg <3031008500108@ingenieria.usac.edu.gt>
2023-09-05 19:27:17 -0600

    commit

--- Clase.py
+++
@@ -1,9 +0,0 @@
-from ListaSimple import ListaSimple
-class Clase:
-    def __init__(self, t, A, dato):
-        self.t = t
-        self.A = A
-        self.dato = dato
-        self.dato = ListaSimple()
-        self.siguiente = None
-        self.anterior = None
